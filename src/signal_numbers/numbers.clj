58125dcfe49d2dab7dfdccc25f49a42dff2f38e8
Krzysztof Kaczmarek <krzysztof.kaczmarek@allegrogroup.com>
2016-04-09 22:54:29 +0200

    stylistic changes

--- src/signal_numbers/numbers.clj
+++ src/signal_numbers/numbers.clj
@@ -12,13 +12,15 @@
   (condp > number
    20  (base-values->words number)
    100 (let [units (mod number 10)
-                        tens (- number units)]
-                    (str (base-values->words tens)
-                         (if (not= 0 units) (str "-" (base-values->words units)))))
+             tens (- number units)]
+         (str (base-values->words tens)
+              (if (not= 0 units)
+                (str "-" (base-values->words units)))))
    1000 (let [tens-with-units (mod number 100)
-              hundreds  (-> number (- tens-with-units) (/ 100))]
+              hundreds  (quot number 100)]
           (str (base-values->words hundreds)
                " hundred"
-               (if (not= 0 tens-with-units) (str " " (int->english-words tens-with-units)))))
+               (if (not= 0 tens-with-units)
+                 (str " " (int->english-words tens-with-units)))))
    1001 "one thousand"
    nil))
